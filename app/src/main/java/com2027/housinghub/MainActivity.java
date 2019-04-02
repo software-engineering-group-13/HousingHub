766bffcc650d8fe044bb8507bfd2e81105dc5c92
Dylan <dp00405@surrey.ac.uk>
2019-04-02 23:59:06 +0100

    Landlord Main (Login) Student Verify Dylan Parker

--- app/src/main/java/com2027/housinghub/MainActivity.java
+++ app/src/main/java/com2027/housinghub/MainActivity.java
@@ -1,7 +1,15 @@
 package com2027.housinghub;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Retrieves data from other activities which return to this activity when a pressable
+        //item is activated. In most cases a button.
+        Intent intent = getIntent();
+        if (intent.getBooleanExtra(StudentActivity.DIALOG_STUDENT, false) || intent.getBooleanExtra(VerifyActivity.DIALOG_LANDLORD, false)) {
+            openDialog(0);
+        }
+
+        //Sets the background to the image by finding the imageView through its id, the image chosen
+        //is in the drawable folder name background
+        ImageView background = (ImageView) findViewById(R.id.imageView);
+        background.setImageResource(R.drawable.background);
+
+        //On press the login button will execute the code contained within the onClick function.
+        Button login = (Button) findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        EditText username = (EditText) findViewById(R.id.editText2);
+        EditText password = (EditText) findViewById(R.id.editText);
+
+        //On press the account textview will execute the code contained within the onClick function.
+        TextView account = (TextView) findViewById(R.id.textView2);
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creates an intent and starts a new activity
+                Intent accountact = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(accountact);
+            }
+        });
+
     }
+
+    /**
+     * Function creates a dialog box, the contents of the box is determined by the inputted
+     * parameter
+     * @param type
+     *          an integer which determins the contents of the dialog box
+     */
+    protected void openDialog(int type) {
+        //Dialog box will be displayed on this activity
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        if (type == 0) {
+            //Sets the title, message and button of the dialog box
+            builder.setTitle("Verification Email Sent!")
+                    .setMessage("Please check your email to verify your account.")
+                    .setPositiveButton("CONFIRM", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Closes the dialog
+                        }
+                    });
+        } else if (type == 1) {
+            //Sets the title, message and button of the dialog box
+            builder.setTitle("Recovery Email Sent!")
+                    .setMessage("Please check your email to recover your account.")
+                    .setPositiveButton("CONFIRM", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Closes the dialog
+                        }
+                    });
+        } else {
+            //Sets the title, message and button of the dialog box
+            builder.setTitle("An error occurred!")
+                    .setMessage("Something happend, our top engineers will fix it soon. Thank you for your patience")
+                    .setPositiveButton("CONFIRM", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //Closes the dialog
+                        }
+                    });
+        }
+        //Shows the dialog box
+        builder.create().show();
+    }
+
 }
---
+++ app/src/main/java/com2027/housinghub/StudentActivity.java
@@ -0,0 +1,96 @@
+package com2027.housinghub;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class StudentActivity extends AppCompatActivity {
+
+    public static final String DIALOG_STUDENT = "";
+    protected Integer REQUEST_CAMERA = 1;
+    protected Integer SELECT_FILE = 0;
+    protected ImageView camera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+
+        //Sets background imageview to the background image within the drawable folder
+        ImageView background = (ImageView) findViewById(R.id.imageView3);
+        background.setImageResource(R.drawable.background);
+
+        //On press the camera imageview will execute the code contained within the onClick function.
+        camera = (ImageView) findViewById(R.id.imageView6);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Opens camera and allows the user to take photos.
+                final CharSequence[] selection = {"Camera", "Gallery"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(StudentActivity.this);
+                builder.setTitle("Select option:")
+                        .setItems(selection, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                if(selection[which].equals("Camera")) {
+                                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                    startActivityForResult(intent, REQUEST_CAMERA);
+                                } else if (selection[which].equals("Gallery")) {
+                                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                    intent.setType("image/*");
+                                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
+                                }
+                            }
+                        });
+                builder.setPositiveButton("CLOSE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Closes the dialog
+                    }
+                });
+                builder.create().show();
+            }
+        });
+
+        //On press the verify button will execute the code contained within the onClick function.
+        Button verify = (Button) findViewById(R.id.button5);
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creates an intent and starts a new activity, returns a boolean value to inform
+                //the login activity to display the dialog box associated with this activity.
+                Intent verifyact = new Intent(StudentActivity.this, MainActivity.class);
+                verifyact.putExtra(DIALOG_STUDENT, true);
+                startActivity(verifyact);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {
+                Bundle bundle = data.getExtras();
+                final Bitmap bmp = (Bitmap) bundle.get("data");
+                camera.setImageBitmap(bmp);
+            } else if (requestCode == SELECT_FILE) {
+                Uri selectImage = data.getData();
+                camera.setImageURI(selectImage);
+            }
+        }
+    }
+
+}
