75049632a1cf8ec74ca70dfed5792618204c8191
Paul <paul.timehin@gmail.com>
2019-04-03 22:33:55 +0100

    Added name, telephone and user type fields to the Landlord

--- app/src/main/java/com2027/housinghub/LandlordActivity.java
+++ app/src/main/java/com2027/housinghub/LandlordActivity.java
@@ -19,12 +19,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LandlordActivity extends AppCompatActivity {
 
     private Button buttonRegister;
     private EditText editTextEmail;
     private EditText editTextPassword;
+
+    private EditText editTextFirstName;
+    private EditText editTextSurname;
+    private EditText editTextPhoneNumber;
 
     private ProgressDialog progressDialog;
 
@@ -45,6 +50,9 @@ public class LandlordActivity extends AppCompatActivity {
 
         editTextEmail = (EditText) findViewById(R.id.etEmailAddress);
         editTextPassword = (EditText) findViewById(R.id.etPassword);
+        editTextFirstName = findViewById(R.id.etFirstname);
+        editTextSurname = findViewById(R.id.etSurname);
+        editTextPhoneNumber = findViewById(R.id.etPhoneNumber);
 
 
         //Sets background imageview to the background image within the drawable folder
@@ -66,8 +74,12 @@ public class LandlordActivity extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = editTextEmail.getText().toString().trim();
+                final String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
+                final String firstName = editTextFirstName.getText().toString().trim();
+                final String surname = editTextSurname.getText().toString().trim();
+                final String phoneNumber = editTextPhoneNumber.getText().toString().trim();
+                final String userType = "Landlord";
 
                 if(TextUtils.isEmpty(email)) {
                     //email is empty
@@ -91,6 +103,24 @@ public class LandlordActivity extends AppCompatActivity {
                     editTextPassword.requestFocus();
                     return;
                 }
+                if (TextUtils.isEmpty(firstName)) {
+                    //password is empty
+                    editTextFirstName.setError("Please Enter Your First Name");
+                    editTextFirstName.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(surname)) {
+                    //password is empty
+                    editTextSurname.setError("Please Enter Your Surname");
+                    editTextSurname.requestFocus();
+                    return;
+                }
+                if (TextUtils.isEmpty(phoneNumber)) {
+                    //password is empty
+                    editTextPhoneNumber.setError("Please Enter Your Phone Number");
+                    editTextPhoneNumber.requestFocus();
+                    return;
+                }
                 // if validations are ok
 
                 progressDialog.setMessage("Registering User...");
@@ -103,7 +133,27 @@ public class LandlordActivity extends AppCompatActivity {
                                 progressDialog.hide();
                                 if (task.isSuccessful()) {
                                     // user is successfully registered
-                                    Toast.makeText(LandlordActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                                    User user = new User(
+                                            firstName,
+                                            surname,
+                                            email,
+                                            userType
+                                    );
+                                    user.setPhone(phoneNumber);
+
+                                    FirebaseDatabase.getInstance().getReference("Users")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()) {
+                                                Toast.makeText(LandlordActivity.this, "User Is Registered", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Toast.makeText(LandlordActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+
                                 } else {
                                     // failed to register
                                     if(task.getException() instanceof FirebaseAuthUserCollisionException) {
